eb856f9b80f433e0555a2667ecbefeadffdf40e2
abhijan2402 <abhishek.jangid643@gmail.com>
2023-07-17 18:30:25 +0530

    Adding some changwe

--- src/database.js
+++ src/database.js
@@ -1,15 +1,15 @@
 const { Client } = require('pg');
 require('dotenv').config();
+const client = new Client({
+    user: 'amezixDB',
+    password: process.env.PASSWORD,
+    host: process.env.CONNECTIONSTRING,
+    database: process.env.DBNANME,
+    port: 5432,
+});
 
 async function connectDB() {
     try {
-        const client = new Client({
-            user: 'amezixDB',
-            password: 'Amezix2020',
-            host: 'database-2.caaxplixixp3.ap-south-1.rds.amazonaws.com',
-            database: 'amezix',
-            port: 5432,
-        })
         client.connect(function (err) {
             if (err) throw err;
             console.log("Connected!");
@@ -19,4 +19,4 @@ async function connectDB() {
     }
 }
 
-module.exports = connectDB;
+module.exports = { connectDB, client };
